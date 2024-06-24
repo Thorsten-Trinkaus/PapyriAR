24dc56becbac4fec70a2ce5a0f82159ac73c9a93
Thorsten Trinkaus <116072811+Thorsten-Trinkaus@users.noreply.github.com>
2024-06-24 15:20:05 +0200

    Update ar.js

--- AR/ar.js
+++ AR/ar.js
@@ -13,6 +13,7 @@ function addLine(text, color, width = 5) {
     textEl.setAttribute("position", {x: 0.75, y: 0, z: 0.5 * lineCount - 0.5});
     textEl.setAttribute("color", color);
     textEl.setAttribute("value", text);
+    textEl.setAttribute("width", toString(width));
     textEl.setAttribute("font", "custom-msdf.json");
     textEl.setAttribute("negate", "false");
     textEl.setAttribute("scale", {x: 6,y: 6,z: 6});
